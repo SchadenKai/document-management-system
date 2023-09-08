7c863450d42bce1ef12e551889f91d4135d1dd8d
SchadenKai <kairustecsonpersonal@gmail.com>
2023-09-08 22:46:22 +0800

    initial commits

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # document-management-system
 Document Management System integrated with various AI models. 
+
+## Technologies used:
+- [ ] React 
+- [ ] Django
+- [ ] Vite 
+- [ ] Typescript
+- [ ] MongoDB
+- [ ] TailwindCSS
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-full h-full p-3">
+      <div className="w-full text-center">
+        <h1 className="text-2xl font-semibold">Document Management System</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
